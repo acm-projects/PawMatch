21e48aef7a6a6e2c5204191ea66780af25595cf9
hxpathak <100104182+hxpathak@users.noreply.github.com>
2022-04-10 21:46:30 -0500

    Finished card flip animation
    Completed card flip animation and added icons to buttons

--- data/animals.js
+++ data/animals.js
@@ -6,6 +6,9 @@ export default [
       image:
         'https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg',
       bio: 'I am super energetic and love to be around kids!',
+      age: 'Young',
+      gender: 'Female',
+      size: 'Small',
     },
     {
       id: '2',
@@ -14,6 +17,9 @@ export default [
       image:
         'https://s36537.pcdn.co/wp-content/uploads/2018/11/persian-cat-face.jpg.optimal.jpg',
       bio: 'I love to take naps',
+      age: 'Adult',
+      gender: 'Male',
+      size: 'Medium',
     },
     {
       id: '3',
@@ -22,6 +28,9 @@ export default [
       image:
         'https://www.rd.com/wp-content/uploads/2020/04/GettyImages-694542042-e1586274805503.jpg',
       bio: 'I love cuddles and carrots!',
+      age: 'Baby',
+      gender: 'Male',
+      size: 'Small',
     },
     {
       id: '4',
@@ -30,6 +39,9 @@ export default [
       image:
         'https://www.thesprucepets.com/thmb/DDnNOYhi5W4tqxiBMwrclOpXCyc=/4239x2384/smart/filters:no_upscale()/ricky-kharawala-adK3Vu70DEQ-unsplash-0fd4bcb628bd49c88d8a023130132a7f.jpg',
       bio: 'I am super friendly',
+      age: 'Young',
+      gender: 'Female',
+      size: 'Small',
     },
     {
       id: '5',
@@ -38,6 +50,9 @@ export default [
       image:
         'https://www.akc.org/wp-content/uploads/2016/06/German-Shepherd-Dog-laying-down-in-the-backyard-500x487.jpeg',
       bio: 'I love playing outside!',
+      age: 'Senior',
+      gender: 'Male',
+      size: 'Large',
     },
 
   ];
